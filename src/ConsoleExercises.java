8c1a6caf740c93e3e28b7aeb7db5e37a48c5be62
J.R <juan.delarosa1224@gmail.com>
2018-11-07 16:10:17 -0600

    solutions to ConsoleExercises.java

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String [] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f", pi);
+//
+        Scanner sc = new Scanner(System.in);
+//        System.out.print("Please enter an integer");
+//        int userInt = sc.nextInt();
+//
+//        System.out.printf("You entered %d%n", userInt);
+//
+//        System.out.print("Please enter three words: ");
+//        String word1 = sc.next();
+//        String word2 = sc.next();
+//        String word3 = sc.next();
+//        System.out.printf("%s %s %s", word1, word2, word3);
+
+//        System.out.print("Enter something: ");
+//        String userInput = sc.nextLine();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+//        System.out.println("please enter length and width?");
+//        int length = Integer.parseInt(sc.nextLine());
+//        int width = Integer.parseInt(sc.nextLine());
+//        System.out.println(length * width);
+//        int area = length * width;
+//        System.out.println((width * 2) + (length * 2));
+//        int perimeter = (width * 2) + (length *2);
+//        System.out.printf("this is your area %d\n", area);
+//        System.out.printf("this is your perimeter %d", perimeter);
+
+        System.out.println(sc);
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,43 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-//        System.out.print("Hello, ");
-//        System.out.print("World!");
+//        System.out.println("Hello World");
+////        System.out.print("Hello World!");
+////        System.out.print("World!");
+int myFavoriteNumber = 3;
+System.out.println(myFavoriteNumber);
+String myString = "this is my string";
+// 'c' does not work, neither does a number value (compiler errors)
+        System.out.println(myString);
+//        long myNumber = 123;
+        float myNumber= 1.23F;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int xy = 5;
+        System.out.println(++xy);
+        System.out.println(xy);
+//       unable to create new variable with class name.
+//
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        System.out.println(o);
+//        int three = (int) o;
+//        int three = (int) "three";
+//        System.out.println(three);
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
     }
 }
