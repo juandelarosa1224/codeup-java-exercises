ea9e29f5682f4aab3cfa40759fd3f0cf1a7eca12
J.R <juan.delarosa1224@gmail.com>
2018-11-08 16:11:56 -0600

    spaced out code

--- src/Bob.java
+++ src/Bob.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String more;
+
         do {
-        System.out.println("Ask Bob a question?");
-        String userInput = sc.nextLine();
+
+            System.out.println("Ask Bob a question?");
+            String userInput = sc.nextLine();
         if (userInput.endsWith("?")) {
             System.out.println("Sure");
         } else if (userInput.endsWith("!")) {
@@ -17,15 +20,10 @@ public class Bob {
             System.out.println("Whatever.");
         }
             System.out.println("Ask more questions?");
+
         more = sc.nextLine();
+
         } while (more.equalsIgnoreCase("yes"));
         System.out.println("bye");
     }
 }
-//    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-//
-//        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-//        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-//        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-//        He answers 'Whatever.' to anything else.
-//        Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
